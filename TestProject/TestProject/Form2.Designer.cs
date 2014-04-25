6efd4078dda6544e7c555b6247dbb049544fe438
Babush <kotevski.vladimir@gmail.com>
2014-04-26 01:46:10 +0200

    Version 1.0

--- TestProject/TestProject/Form2.Designer.cs
+++ TestProject/TestProject/Form2.Designer.cs
@@ -36,6 +36,7 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -44,8 +45,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 76);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "7";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -53,8 +55,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(83, 76);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "6";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -62,8 +65,9 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(83, 76);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "2";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -71,8 +75,9 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(83, 76);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "3";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -80,8 +85,9 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(83, 76);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -89,8 +95,9 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(83, 76);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "4";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -98,8 +105,9 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(83, 76);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
+            this.button7.Text = "1";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -107,14 +115,25 @@
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(83, 76);
             this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(190, 173);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(83, 76);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -139,6 +158,7 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
 
 
     }
--- TestProject/TestProject/Form2.cs
+++ TestProject/TestProject/Form2.cs
@@ -17,9 +17,175 @@ namespace TestProject
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            if (button2.Text == "")
+            {
+                button2.Text = button1.Text;
+                button1.Text = "";
+            }
+            if (button4.Text == "")
+            {
+                button4.Text = button1.Text;
+                button1.Text = "";
+            }
+            proveri();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "")
+            {
+                button1.Text = button2.Text;
+                button2.Text = "";
+            }
+            if (button3.Text == "")
+            {
+                button3.Text = button2.Text;
+                button2.Text = "";
+            }
+            if (button5.Text == "")
+            {
+                button5.Text = button2.Text;
+                button2.Text = "";
+            }
+            proveri();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "")
+            {
+                button2.Text = button3.Text;
+                button3.Text = "";
+            }
+            if (button6.Text == "")
+            {
+                button6.Text = button3.Text;
+                button3.Text = "";
+            }
+            proveri();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "")
+            {
+                button1.Text = button4.Text;
+                button4.Text = "";
+            }
+            if (button5.Text == "")
+            {
+                button5.Text = button4.Text;
+                button4.Text = "";
+            }
+            if (button7.Text == "")
+            {
+                button7.Text = button4.Text;
+                button4.Text = "";
+            }
+            proveri();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "")
+            {
+                button2.Text = button5.Text;
+                button5.Text = "";
+            }
+            if (button4.Text == "")
+            {
+                button4.Text = button5.Text;
+                button5.Text = "";
+            }
+            if (button6.Text == "")
+            {
+                button6.Text = button5.Text;
+                button5.Text = "";
+            }
+            if (button8.Text == "")
+            {
+                button8.Text = button5.Text;
+                button5.Text = "";
+            }
+            proveri();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "")
+            {
+                button3.Text = button6.Text;
+                button6.Text = "";
+            }
+            if (button5.Text == "")
+            {
+                button5.Text = button6.Text;
+                button6.Text = "";
+            }
+            if (button9.Text == "")
+            {
+                button9.Text = button6.Text;
+                button6.Text = "";
+            }
+            proveri();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "")
+            {
+                button4.Text = button7.Text;
+                button7.Text = "";
+            }
+            if (button8.Text == "")
+            {
+                button8.Text = button7.Text;
+                button7.Text = "";
+            }
+            proveri();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "")
+            {
+                button7.Text = button8.Text;
+                button8.Text = "";
+            }
+            if (button5.Text == "")
+            {
+                button5.Text = button8.Text;
+                button8.Text = "";
+            }
+            if (button9.Text == "")
+            {
+                button9.Text = button8.Text;
+                button8.Text = "";
+            }
+            proveri();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "")
+            {
+                button8.Text = button9.Text;
+                button9.Text = "";
+            }
+            if (button6.Text == "")
+            {
+                button6.Text = button9.Text;
+                button9.Text = "";
+            }
+            proveri();
+        }
+        public void proveri()
+        {
+            if (button1.Text == "1" && button2.Text == "2" && button3.Text == "3" && button4.Text == "4" &&
+                button5.Text == "5" && button6.Text == "6" && button7.Text == "7" && button8.Text == "8")
+                MessageBox.Show("Bravo");
         }
     }
 }
