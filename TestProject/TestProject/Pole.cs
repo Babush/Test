cf31b6bd12f6c584f7a9f83dd7a11463525801cd
Babush <kotevski.vladimir@gmail.com>
2014-04-28 16:43:55 +0200

    Klasa Pole

---
+++ TestProject/TestProject/Pole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Pole
+    {
+        public int Broj { get; set; }
+        public string Slika { get; set; }
+        public Pole(int broj,string slika)
+        {
+            Broj = broj;
+            Slika = slika;
+        }
+    }
+}
